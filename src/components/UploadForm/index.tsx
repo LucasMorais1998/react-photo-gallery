8a4006de9af272e5bdeb426afba1906038e92579
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-23 23:05:46 -0300

    add UploadForm features

--- src/components/UploadForm/index.tsx
+++ src/components/UploadForm/index.tsx
@@ -1,6 +1,9 @@
 import { FormEvent } from "react";
 
-import { Container } from "./styles";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { MdAddAPhoto } from "react-icons/md";
+
+import { Container, LoadingContainer } from "./styles";
 
 interface IUploadFormProps {
   onSubmitForm: (e: FormEvent<HTMLFormElement>) => void;
@@ -10,9 +13,22 @@ interface IUploadFormProps {
 const UploadForm = ({ onSubmitForm, isUploading }: IUploadFormProps) => {
   return (
     <Container method="POST" onSubmit={onSubmitForm}>
-      <input type="file" name="image" />
-      <input type="submit" value="Enviar" />
-      {isUploading && "Enviando ..."}
+      <label id="labelInputImage" htmlFor="inputImage">
+        <MdAddAPhoto className="add-image-icon" />
+        <input id="inputImage" type="file" name="image" />
+      </label>
+
+      <label id="labelInputSubmit" htmlFor="inputSubmit">
+        <input id="inputSubmit" type="submit" value="Enviar" />
+      </label>
+
+
+      {isUploading && (
+        <LoadingContainer>
+          <AiOutlineLoading3Quarters className="loading-icon" />
+          <span>Enviando Imagem...</span>
+        </LoadingContainer>
+      )}
     </Container>
   );
 };
