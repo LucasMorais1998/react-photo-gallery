a1f853991deea7a7954ca16afa2e8e34d8555bb0
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-23 21:40:43 -0300

    add UploadForm styles

--- src/components/UploadForm/styles.ts
+++ src/components/UploadForm/styles.ts
@@ -1,3 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.form``;
+export const Container = styled.form`
+  background-color: var(--gray-color);
+  padding: 0.938rem;
+  border-radius: 0.625rem;
+  margin-bottom: 1.875rem;
+
+  input[type="submit"] {
+    background-color: var(--purple-color);
+    border: 0;
+    color: var(--white-color);
+    padding: 0.5rem 1rem;
+    font-size: 0.938rem;
+    border-radius: 0.625rem;
+    margin: 0 1.25rem;
+    cursor: pointer;
+    opacity: 0.5;
+    transition: opacity ease 1s;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
