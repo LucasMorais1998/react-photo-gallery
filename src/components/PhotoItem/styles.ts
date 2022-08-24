c5f839a12cae663fdaa5e14ccad981144df9f62f
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-23 21:28:32 -0300

    add PhotoItem styles

--- src/components/PhotoItem/styles.ts
+++ src/components/PhotoItem/styles.ts
@@ -1,3 +1,25 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background-color: var(--gray-color);
+  border-radius: 0.625rem;
+  padding: 0.625rem;
+  
+  max-height: 12rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+
+  img {
+    width: 100%;
+    height: 85%;
+    max-width: 100%;
+    border-radius: 0.625rem;
+    object-fit: cover;
+  }
+
+  span {
+    text-align: center;
+    letter-spacing: 1px;
+  }
+`;
