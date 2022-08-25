723333b84b4c33d3e45e669e7a3f133b84e2aa2f
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-25 17:54:06 -0300

    add DeleteModal component

---
+++ src/components/DeleteModal/index.tsx
@@ -0,0 +1,45 @@
+import Modal from "react-modal";
+
+import { FaWindowClose } from "react-icons/fa";
+import { IoMdCheckmark, IoMdClose } from "react-icons/io";
+
+interface DeleteModalProps {
+  isOpen: boolean;
+  photoName: string;
+  onRequestClose: () => void;
+  onDelete: (name: string) => void;
+}
+
+const DeleteModal = ({
+  isOpen,
+  photoName,
+  onRequestClose,
+  onDelete,
+}: DeleteModalProps) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <FaWindowClose onClick={onRequestClose} className="react-modal-close" />
+
+      <p className="react-modal-message">
+        Deseja deletar permanentemente a imagem?
+      </p>
+      <div className="react-modal-buttons">
+        <IoMdCheckmark
+          className="react-modal-confirm-true"
+          onClick={() => onDelete(photoName)}
+        />
+        <IoMdClose
+          className="react-modal-confirm-false"
+          onClick={onRequestClose}
+        />
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
