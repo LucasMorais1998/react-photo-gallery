ba40cef3507ac3a27629d40e7c3301e49e653017
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-25 17:55:13 -0300

    feat DeleteModal

--- src/components/PhotoItem/index.tsx
+++ src/components/PhotoItem/index.tsx
@@ -1,15 +1,43 @@
+import { MdDeleteForever } from "react-icons/md";
+
+import DeleteModal from "../../components/DeleteModal";
+
 import { Container } from "./styles";
 
 interface IPhotoItemProps {
   name: string;
   url: string;
+  isOpen: boolean;
+  onOpenDeleteModal: () => void;
+  onRequestClose: () => void;
+  onDelete: (name: string) => void;
 }
 
-const PhotoItem = ({ name, url }: IPhotoItemProps) => {
+const PhotoItem = ({
+  name,
+  url,
+  isOpen,
+  onOpenDeleteModal,
+  onRequestClose,
+  onDelete,
+}: IPhotoItemProps) => {
   return (
     <Container>
       <img src={url} alt={name} />
-      <span>{name}</span>
+      <div className="photo-item">
+        <span>{name}</span>
+        <MdDeleteForever
+          className="photo-item-delete-icon"
+          onClick={onOpenDeleteModal}
+        />
+      </div>
+
+      <DeleteModal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        onDelete={onDelete}
+        photoName={name}
+      />
     </Container>
   );
 };
